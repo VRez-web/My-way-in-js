9a02470ccb93a8e97e02cdc852add8f94b05271e
VRez-web <56605107+VRez-web@users.noreply.github.com>
2020-05-17 12:59:37 +0300

    New knowledge

--- base-js/04-functions.js
+++ base-js/04-functions.js
@@ -69,3 +69,118 @@
 // const logWithLastName= createMember('Victor')
 // console.log(logWithLastName('rez'))
 
+/*========Рекурсия
+– это термин в программировании, означающий вызов функцией самой себя.
+ Рекурсивные функции могут быть использованы для элегантного решения определённых задач.
+*/
+//===========Задачи
+/*....Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n.
+
+Сделайте три варианта решения:
+
+С использованием цикла.
+Через рекурсию, т.к. sumTo(n) = n + sumTo(n-1) for n > 1.
+С использованием формулы арифметической прогрессии.
+*/
+
+// const sumTo = n => {
+//     let sum = 0
+//     for (let i = 0; i <= n; i++) {
+//         sum += i
+//     }
+//     return sum
+// }
+
+// const sumTo = n => (n == 1) ? n : n + sumTo(n - 1)
+
+// console.log(sumTo(100))
+
+/*....Написать функцию factorial(n), которая возвращает n!, используя рекурсию.
+ */
+
+// const factorial = n => (n != 1) ? n * factorial(n - 1) : 1
+// console.log(factorial(5))
+
+/*.....Напишите функцию fib(n) которая возвращает n-е число Фибоначчи.
+ */
+// const fib = n => (n <= 1) ? n:  fib(n - 1) + fib(n - 2)
+
+// console.log(fib(7))
+
+
+// let list = {
+//     value: 1,
+//     next: {
+//         value: 2,
+//         next: {
+//             value: 3,
+//             next: {
+//                 value: 4,
+//                 next: null
+//             }
+//         }
+//     }
+// }
+
+//....Напишите функцию printList(list), которая выводит элементы списка по одному.
+
+// const printList = list => {
+//     console.log(list.value)
+
+//     if (list.next) {
+//         printList(list.next)
+//     }
+// }
+// printList(list)
+
+//============Замыкания
+// function createCalcFunction(n) {
+//     return function () {
+//             console.log(1000*n)
+
+//     }
+// }
+// const calc= createCalcFunction(42)
+
+// calc()
+
+// function createIncrementor(n) {
+//     return function (num) {
+//         return n + num
+//     }
+// }
+// const addOne = createIncrementor(1)
+// const addTen = createIncrementor(10)
+// console.log(addOne(10))
+// console.log(addOne(41))
+
+// console.log(addTen(10))
+// console.log(addTen(41))
+
+// function urlGenerator(domain) {
+//     return function (url) {
+//         return `https://${url}.${domain}`
+//     }
+// }
+// const comUrl = urlGenerator('com')
+// const ruUrl = urlGenerator('ru')
+// console.log(comUrl('google'))
+// console.log(comUrl('netflix'))
+
+// console.log(ruUrl('yandex'))
+// console.log(ruUrl('vkontakte'))
+
+
+
+//===Задачи===
+/*Напишите функцию sum, которая работает таким образом: sum(a)(b) = a+b.
+ */
+
+// function sum(a) {
+
+//     return function (b) {
+//         return a + b
+//     }
+// }
+
+// console.log(sum(3)(5))
