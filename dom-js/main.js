f7fe24086e3dde86c56de1cecafcbc3187b3e2f1
VRez-web <56605107+VRez-web@users.noreply.github.com>
2020-05-20 20:07:38 +0300

    new version

--- dom-js/main.js
+++ dom-js/main.js
@@ -1,21 +1,5 @@
-/*=============Задачи
+let name = prompt("Введите ваше имя?", "<b>Винни-пух!</b>");
 
-Как найти?…
-
-Таблицу с id="age-table".
-Все элементы label внутри этой таблицы (их три).
-Первый td в этой таблице (со словом «Age»).
-Форму form с именем name="search".
-Первый input в этой форме.
-Последний input в этой форме.
-*/
-
-const ageTable = document.getElementById('age-table')
-const form = document.getElementsByName('search')
-let inputs = form.querySelectorAll('input')
-
-ageTable.style.background = 'red'
-ageTable.getElementsByTagName('label')
-ageTable.getElementsByTagName('td')[0]
-inputs[inputs.length - 1]
-form.getElementsByTagName('input')[0]
+elem1.innerHTML = name;
+elem2.textContent = name;
+elem2.hidden = true
