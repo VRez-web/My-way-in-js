788dbcaa595e6d71135f4ef9b11ea83d4c44676d
VRez-web <56605107+VRez-web@users.noreply.github.com>
2020-06-06 10:45:46 +0300

    mini-rework

--- application/YetiFilms/src/index.js
+++
@@ -1,9 +0,0 @@
-// Styles
-import './assets/scss/style.scss'
-import './assets/theme/theme.css'
-import 'normalize.css'
-
-
-// Scripts
-import {dbService} from './assets/js/db-service'
-import './assets/js/events'
